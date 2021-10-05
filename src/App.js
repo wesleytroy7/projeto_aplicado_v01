e241bdf0f5ff9a4b7fc4082e974513bc184fd030
Wesley AF <3717727+wesleytroy7@users.noreply.github.com>
2021-10-04 22:50:27 -0300

    crud  video 16:30

--- src/App.js
+++ src/App.js
@@ -6,51 +6,24 @@ import Main from './componets/Main';
 import TextInput from './componets/TextInput';
 import getNewId from './services/idServices';
 import Button from './componets/Button';
+import mock_data from './mock-data.json';
+import { Fragment } from 'react/cjs/react.production.min';
+import Input from './componets/Input';
 export default function App() {
   //useState pesagem inputText
-  const [weight, setWeight] = useState('00.00');
-
-  function handleWeightChange(newWeight) {
-    setWeight(newWeight);
-  }
 
   return (
-    <>
-      <Header>UTI</Header>
-      <Main>
-        <form className="flex flex-wrap p-4">
-          <TextInput
-            id={getNewId()}
-            labelDescription="PESO ROUPA LEVE:"
-            typeInput="number"
-            inputPlaceHolder="00.00"
-            inputValue={weight}
-            onInputChange={handleWeightChange} //quando trocar o texto
-            autoFocus
-          />
-          <Button>Incluir</Button>
-        </form>
+    <Fragment>
+      <Main className="posts">
+        {mock_data.map(post => {
+          return (
+            <div key={post.id} className="post">
+              <h3>{post.id}</h3>
+              <h3>{post.peso}</h3>
+            </div>
+          );
+        })}
       </Main>
-
-      <p className="mt-4 text-xl text-gray-500">{weight}</p>
-      <table className="w-full divide-y divide-gray-200">
-        <thead className="bg-gray-200">
-          <th className="px-6 py-3 text-center text-4xl font-bold text-gray-900 uppercase border-2 border-gray-600">
-            PESO/Kg
-          </th>
-        </thead>
-        <tbody className="px-6 py-3 text-center text-4xl font-bold text-gray-900 uppercase tracking-wider whitespace-nowrap border-4">
-          <tr className="border-2">
-            <td>12.20</td>
-          </tr>
-          <tr className="border-2">
-            <td>10.0</td>
-          </tr>
-          <tr className="border-2">
-            <td>9.30</td>
-          </tr>
-        </tbody>
-      </table>
-    </>
+    </Fragment>
   );
 }
